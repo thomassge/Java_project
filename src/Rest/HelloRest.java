6597a4450dac23748981cb07f4a6345054150485
thomassge <thomas.levantis@stud.fra-uas.de>
2023-12-14 16:43:31 +0100

    HelloRest muss in ein package!

--- src/Rest/HelloRest.java
+++ src/Rest/HelloRest.java
@@ -1,3 +1,4 @@
+package Rest;
 
 import java.io.BufferedReader;
 import java.io.IOException;
--- src/module-info.java
+++ src/module-info.java
@@ -4,6 +4,8 @@
 /**
  * 
  */
-module JProject {
+
+module Java_project {
     requires org.json;
+    exports Rest;
 }
